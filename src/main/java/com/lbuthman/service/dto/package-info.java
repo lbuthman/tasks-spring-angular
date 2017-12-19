265fd546f16477682b09865f91f93211cce00f59
Luke Buthman <lbuthman@gmail.com>
2017-12-19 05:34:32 -0600

    [chore] Init commit

---
+++ src/main/java/com/lbuthman/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.lbuthman.service.dto;
---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,54 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ngx-webstorage';
+
+import { TasksSharedModule, UserRouteAccessService } from './shared';
+import { TasksAppRoutingModule} from './app-routing.module';
+import { TasksHomeModule } from './home/home.module';
+import { TasksAdminModule } from './admin/admin.module';
+import { TasksAccountModule } from './account/account.module';
+import { TasksEntityModule } from './entities/entity.module';
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import {
+    JhiMainComponent,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        TasksAppRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        TasksSharedModule,
+        TasksHomeModule,
+        TasksAdminModule,
+        TasksAccountModule,
+        TasksEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class TasksAppModule {}
